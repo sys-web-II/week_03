439d111f7f42cf055c86d619703f01813bdc7036
Tenshi . <jrcontreras446@gmail.com>
2025-06-06 19:00:09 -0600

    Initial commit, added proyect body

---
+++ week_03/src/App.tsx
@@ -0,0 +1,23 @@
+function App() {
+			  return (
+				<div className="container">
+					<h1>Hola mundo</h1>
+					<p>
+						Esto es solo una prueba de lo que podemos realizar con React Web. <br/>
+						Esto incluye el uso de Bootstrap, el redireccionamiento entre las páginas del sitio web, así como el uso de hojas de estilo.
+						<br/>
+						<b>Usando JavaScript con el estándar ECMAScript 6 (ES6).</b>
+						<br/>
+						Entre los temas que se abordarán en esta clase se encuentran:
+					</p>
+					<ul>
+						<li>Declaración de variables y constantes.</li>
+						<li>Estructuras de control.</li>
+						<li>Declaración y uso de funciones.</li>
+						<li>Fundamentos de Programación Orientada a Objetos.</li>
+					</ul>
+				</div>
+			  );
+			}
+
+export default App
